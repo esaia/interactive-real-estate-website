536f4fe2c082c5c57f4e4d36933014056537b9ff
esaia <esaiagafrindashvili@gmail.com>
2025-01-29 17:34:26 +0400

    refactor: add freemius cdn for account dashboards

--- app.vue
+++ app.vue
@@ -2,7 +2,11 @@
 import "ire-preview/dist/styles.css";
 
 useHead({
-  script: [{ src: "https://checkout.freemius.com/js/v1/", defer: true }],
+  script: [
+    { src: "https://code.jquery.com/jquery-3.7.1.min.js", defer: true },
+    { src: "https://checkout.freemius.com/js/v1/", defer: true },
+    { src: "  https://users.freemius.com/dashboard.js", defer: true },
+  ],
 });
 </script>
 
