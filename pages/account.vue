47e264fb1fd1e59e2d10f13cf0e35e37edb4021b
esaia <esaiagafrindashvili@gmail.com>
2025-01-29 19:30:50 +0400

    test accounts page iframe

--- pages/account.vue
+++ pages/account.vue
@@ -1,12 +1,29 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const account = ref();
+
+onMounted(() => {
+  const script = document.createElement("script");
+  script.src = "https://users.freemius.com/dashboard.js";
+  script.onload = () => {
+    FS.Members.configure({
+      store_id: 8007,
+      public_key: "pk_b32358e8552d82fa6462620256b20",
+      position: "fixed",
+      left: "0",
+      right: "0",
+      top: "195px",
+      bottom: "0",
+    }).open();
+  };
+
+  if (!account.value) return;
+
+  document.body.appendChild(script);
+});
+</script>
 
 <template>
-  <div class="container-fluid">
-    <iframe
-      src="https://users.freemius.com/store/8007/login?public_key=pk_b32358e8552d82fa6462620256b20"
-      class="h-[700px] w-full"
-      width="100%"
-      height="100%"
-    ></iframe>
+  <div ref="account" class="container-fluid h-[600px]">
+    <!-- Dashboard will load dynamically in your app -->
   </div>
 </template>
