6b999c16e29795605307d1ed010c8fd3ece82f19
esaia <esaiagafrindashvili@gmail.com>
2025-01-29 20:16:56 +0400

    feat: accounts page

--- pages/account.vue
+++ pages/account.vue
@@ -1,29 +1,42 @@
 <script setup lang="ts">
-const account = ref();
+import { ref, onMounted } from "vue";
+
+const account = ref<HTMLDivElement | null>(null);
 
 onMounted(() => {
-  const script = document.createElement("script");
-  script.src = "https://users.freemius.com/dashboard.js";
-  script.onload = () => {
-    FS.Members.configure({
-      store_id: 8007,
-      public_key: "pk_b32358e8552d82fa6462620256b20",
-      position: "fixed",
-      left: "0",
-      right: "0",
-      top: "195px",
-      bottom: "0",
-    }).open();
-  };
+  FS?.Members?.configure({
+    store_id: 8007,
+    public_key: "pk_b32358e8552d82fa6462620256b20",
+    left: "0",
+    right: "0",
+    top: "195px",
+    bottom: "0",
+  }).open();
 
-  if (!account.value) return;
+  const iframe: HTMLDivElement | null = document.querySelector(
+    "#fs_dashboard_container",
+  );
 
-  document.body.appendChild(script);
+  if (iframe && account.value) {
+    iframe.style.display = "none";
+
+    account.value.appendChild(iframe);
+
+    iframe.style.display = "block";
+  }
 });
 </script>
 
 <template>
-  <div ref="account" class="container-fluid h-[600px]">
-    <!-- Dashboard will load dynamically in your app -->
+  <div class="container-fluid">
+    <div ref="account" class="relative h-[600px]"></div>
+    <p class="editor py-10">
+      If you can't see the login form or encounter any issues, please visit the
+      <a href="https://users.freemius.com/" target="_blank"
+        >Freemius Members Dashboard</a
+      >
+      for more details or assistance. If the problem persists, feel free to
+      contact support.
+    </p>
   </div>
 </template>
