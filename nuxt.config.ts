4b7aa3dfe4cc511ab13da3e28812e27ba2a590f0
esaia <esaiagafrindashvili@gmail.com>
2025-03-04 18:34:12 +0400

    font optimization

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,35 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
+
+  app: {
+    head: {
+      link: [
+        {
+          rel: "preload",
+          href: "/assets/fonts/Poppins-Regular.ttf",
+          as: "font",
+          type: "font/ttf",
+          crossorigin: "anonymous",
+        },
+        {
+          rel: "preload",
+          href: "/assets/fonts/Poppins-Bold.ttf",
+          as: "font",
+          type: "font/ttf",
+          crossorigin: "anonymous",
+        },
+        {
+          rel: "preload",
+          href: "/assets/fonts/Poppins-SemiBold.ttf",
+          as: "font",
+          type: "font/ttf",
+          crossorigin: "anonymous",
+        },
+      ],
+    },
+  },
+
   devServer: {
     port: 4040,
     host: "0.0.0.0",
@@ -17,7 +46,12 @@ export default defineNuxtConfig({
     // typeCheck: true,
   },
 
-  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/sitemap", "@nuxtjs/device"],
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/sitemap",
+    "@nuxtjs/device",
+    "@nuxtjs/fontaine",
+  ],
 
   sitemap: {
     // inferStaticPagesAsRoutes: false,
@@ -26,9 +60,5 @@ export default defineNuxtConfig({
       { label: "URL", width: "75%" },
       { label: "Last Modified", select: "sitemap:lastmod", width: "25%" },
     ],
-  },
-
-  app: {
-    pageTransition: { name: "page", mode: "out-in" },
   },
 });
