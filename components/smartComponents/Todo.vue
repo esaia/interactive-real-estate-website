31c63f4c32fe80b54e5290014abdbad406fdc44f
esaia <esaiagafrindashvili@gmail.com>
2025-03-24 16:28:54 +0400

    chore: changes in to do

--- components/smartComponents/Todo.vue
+++ components/smartComponents/Todo.vue
@@ -3,9 +3,9 @@
   <div class="container-fluid padding-top max-w-[800px]">
     <h2 class="title text-center">To <span class="gradient-text"> do</span></h2>
     <div class="flex flex-col gap-2 px-2 pt-10 lg:px-8 lg:text-lg">
-      <p class="list">Project importing/exporting</p>
+      <p class="list line-through opacity-60">Project importing/exporting</p>
       <p class="list">Multiple currencies</p>
-      <p class="list">Reservation / booking apartment feature</p>
+      <p class="list">Reservation/booking apartment feature</p>
       <p class="list">More tooltip variants</p>
       <p class="list">Copy apartment link for sharing</p>
     </div>
