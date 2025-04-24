e522d6f62cdfdc3b5e3faa972ce882877627596d
esaia <esaiagafrindashvili@gmail.com>
2025-04-24 14:22:52 +0400

    fix: demo sitemap urls

--- server/api/__sitemap__/urls.ts
+++ server/api/__sitemap__/urls.ts
@@ -4,6 +4,13 @@ import type { SitemapUrlInput } from "#sitemap/types";
 export default defineSitemapEventHandler(async (event) => {
   const articles = (await queryCollection(event, "blog").all()) || [];
   const docs = (await queryCollection(event, "doc").all()) || [];
+  const demosSlug = [
+    "cottages",
+    "hotel",
+    "big-hotel",
+    "entertainment-spaces",
+    "blocks",
+  ];
 
   const blogUrls = articles.map((item) => {
     return {
@@ -21,16 +28,13 @@ export default defineSitemapEventHandler(async (event) => {
     };
   });
 
-  const demoUrls = new Array(5)
-    .fill(0)
-    .map((_, i) => i + 1)
-    .map((i) => {
-      return {
-        loc: `/demos/${i}`,
-        lastmod: new Date(),
-        _sitemap: "pages",
-      };
-    });
+  const demoUrls = demosSlug.map((slug) => {
+    return {
+      loc: `/demos/${slug}`,
+      lastmod: new Date(),
+      _sitemap: "pages",
+    };
+  });
 
   const data = [...blogUrls, ...demoUrls, ...docUrls];
 
