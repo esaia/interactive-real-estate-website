acde895934818fb8487b9b4a4f1cd27ac7ca0a0a
esaia <esaiagafrindashvili@gmail.com>
2025-04-26 12:24:35 +0400

    chore: change demo video

--- components/smartComponents/VideoDemo/VideoDemo.vue
+++ components/smartComponents/VideoDemo/VideoDemo.vue
@@ -3,7 +3,7 @@
     <iframe
       width="100%"
       height="486"
-      src="https://www.youtube-nocookie.com/embed/rRL3usI9N9E?si=ThOoViHNFWWENsD1"
+      src="https://www.youtube.com/embed/HmF62v03PQw?si=Y4uGEGsEGVG_Kpep"
       title="YouTube video player"
       frameborder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
