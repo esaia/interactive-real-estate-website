8086c9fb933ea27712d13b6152fe739130294c96
esaia <esaiagafrindashvili@gmail.com>
2025-09-01 16:34:51 +0400

    fix: change youtube videos to vimeo

--- components/smartComponents/VideoDemo/VideoDemo.vue
+++ components/smartComponents/VideoDemo/VideoDemo.vue
@@ -3,7 +3,7 @@
     class="relative m-auto flex h-full w-full items-center justify-center pt-[56%]"
   >
     <div class="absolute left-0 top-0 h-full w-full">
-      <iframe
+      <!-- <iframe
         width="100%"
         height="100%"
         src="https://www.youtube-nocookie.com/embed/HmF62v03PQw?si=Y4uGEGsEGVG_Kpep"
@@ -13,7 +13,18 @@
         referrerpolicy="strict-origin-when-cross-origin"
         allowfullscreen
         loading="lazy"
-      ></iframe>
+      ></iframe> -->
+
+      <div style="padding: 53.09% 0 0 0; position: relative">
+        <iframe
+          src="https://player.vimeo.com/video/1114859127?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
+          frameborder="0"
+          allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media; web-share"
+          referrerpolicy="strict-origin-when-cross-origin"
+          style="position: absolute; top: 0; left: 0; width: 100%; height: 100%"
+          title="Interactive real estate plugin flow"
+        ></iframe>
+      </div>
     </div>
   </div>
 </template>
